951cf7de1302e3d95daba021b320779cd0361bd7
vittosba <vitsbardella@gmail.com>
2022-03-14 13:01:36 +0100

    ADD: class utilities

--- Utilities.java
+++ Utilities.java
@@ -1,5 +1,23 @@
 // import java.lang.Enum;
 
-// public enum tipo_Ammessi {
-//     evento_musicale, evento_teatrale, evento_comico, evento_sportivo
-// }
+public class Utility {
+    private static Utility istanza = null;
+    private Utility() {}
+    public static Utility getMyUtility() {
+        if (istanza == null)
+            istanza = new Utility();
+            return istanza;
+    }
+    public enum tipo_Ammessi {
+        evento_musicale, evento_teatrale, evento_comico, evento_sportivo
+    }
+    public Boolean email_valida (String email) {
+        Boolean email_valida = false;
+        if(email.contains('@')) {
+            // @ in che posizione?
+            // sottostringa da @ in poi
+            // se in sottostringa contains '.' --> email_valida = true
+        }
+        return email_valida;
+    }
+}
