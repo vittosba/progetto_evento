177ec8573b5f271c240d538ed10a6f6f92ab48c5
vittosba <vitsbardella@gmail.com>
2022-03-15 17:24:59 +0100

    ADD: write and read on file

--- Class/Main.java
+++ Class/Main.java
@@ -1,4 +1,7 @@
 package Class;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Scanner;
 class Main{
 
@@ -20,7 +23,7 @@ class Main{
 
         if (scelta.compareTo("si") == 0){
             //Registrazione
-            
+
             String email_utente;
             do {
                 System.out.println("Inserire email: ");
@@ -39,14 +42,55 @@ class Main{
             do {
                 System.out.println("Inserire password: ");
                 password_utente = in.next();
-                System.out.println(U.verificaPassword(password_utente));
             } while (!U.verificaPassword(password_utente));
 
             // verifica registrazione tramite file
             
             Utente newUtente = new Utente(nome_utente, cognome_utente, email_utente, email_verificata_utente, password_utente);
 
+            String nomeFile = "archivio.txt";
+            try {
+                FileWriter fw = new FileWriter(nomeFile, true);
+                String linea = newUtente.Nome() + " " + newUtente.Cognome() + " " + newUtente.Email() + " " + newUtente.Password()+ " ";
+                fw.write(linea + "\r\n");
+                fw.close();
+            } catch (Exception e) {
+            }
+    
+
             System.out.println( newUtente.Nome() + " " + newUtente.Cognome() + " " + newUtente.Email());
+
+            
+            try{
+                FileReader fr = new FileReader(nomeFile);
+                BufferedReader br = new BufferedReader(fr);
+                String linea;
+                while((linea = br.readLine())!=null){
+                    int spazio1 = linea.indexOf(" ");
+                    String nome = linea.substring(0,spazio1);
+
+                    int spazio2 = linea.indexOf(" ");
+                    String cognome = linea.substring(spazio1,spazio2);
+                    System.out.println(nome + cognome);
+
+                    int spazio3 = linea.indexOf(" ");
+                    String mail = linea.substring(spazio2,spazio3);
+                    int spazio4 = linea.indexOf(" ");
+                    String password = linea.substring(spazio3,spazio4);
+
+                    Utente visualizzaUtente = new Utente(nome, cognome, mail, true, password);
+
+                    if (visualizzaUtente == newUtente) {
+                        System.out.println("Read and Write working");
+                    }
+                    else {
+                        System.out.println("Read and Write not working");
+                    }
+                }
+                br.close();
+                }catch(Exception e){
+                }
+
         }
         in.close();
         System.out.println(" Vuoi Prenotarti? " + "Inserire si o no");
