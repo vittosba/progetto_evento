0ed52c5c7ea8a10b842b6fbf98e78ad40ece7e7e
Lspagnoli02 <lspagnoli02@gmail.com>
2022-03-22 12:07:28 +0100

    bella roba

--- Classi/Utente.java
+++ Classi/Utente.java
@@ -1,11 +1,13 @@
 package Classi;
-public class Utente {
+public class Utente { 
+    /**attributi dell'utente */
     private String nome;
     private String cognome;
     private String email;
     private Boolean email_verificata;
     private String password;
-    
+
+    /**metodo costruttore */
     public Utente(String nome_utente, String cognome_utente, String email_utente, Boolean email_verificata_utente, String password_utente) {
         nome = nome_utente;
         cognome = cognome_utente;
@@ -13,19 +15,19 @@ public class Utente {
         password = password_utente;
     }
 
-    public String restituisceNome() {
+    public String restituisceNome() {   /**restituisce il nome dell'utente*/
         return nome;
     }
-    public String restituisceCognome() {
+    public String restituisceCognome() {    /**restituisce il cognome dell'utente*/
         return cognome;
     }
-    public String restituisceEmail() {
+    public String restituisceEmail() {  /**restituisce l'email dell'utente*/
         return email;
     }
-    public Boolean restituisceVerifica() {
+    public Boolean restituisceVerifica() {  /**verifica che l'email dell'utente sia corretta*/
         return email_verificata;
     }
-    public String restituiscePassword() {
+    public String restituiscePassword() {   /**restituisce la password dell'utente*/
         return password;
     }
 }
